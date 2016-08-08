201722be8e1dfd491a8b89e50b8ed4edb1be61db
zhenjiaWang <zhenjiaWang@gmail.com>
2016-08-08 16:29:04 +0800

    123

--- src/restful/oa/mingdao/com/CommonController.java
+++ src/restful/oa/mingdao/com/CommonController.java
@@ -1,6 +1,7 @@
 package restful.oa.mingdao.com;
 
 import com.google.inject.Inject;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import oa.mingdao.com.entity.SysProvince;
 import oa.mingdao.com.service.SysProvinceService;
@@ -9,6 +10,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,29 +18,24 @@ import java.util.List;
  * Created by zhenjiaWang on 16/8/4.
  */
 @Path("/wzj")
+@Produces(MediaType.APPLICATION_JSON+ ";charset=utf-8")
 public class CommonController {
 
     @Inject
     private SysProvinceService sysProvinceService;
 
+    @Path("/index")
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String index(){
-        return "hello";
+        JSONArray jsonArray=new JSONArray();
+        List<SysProvince> sysProvinceList= sysProvinceService.getList();
+        if(sysProvinceList!=null&&!sysProvinceList.isEmpty()){
+            SysProvince sysProvince=  sysProvinceList.get(0);
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("id", sysProvince.getId()+"");
+            jsonObject.put("name", sysProvince.getName());
+            jsonArray.add(jsonObject);
+        }
+        return jsonArray.toString();
     }
-
-//    @Path("/home")
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public HashMap home(){
-//        HashMap<String, String> map = new HashMap<String, String>();
-//        List<SysProvince> sysProvinceList= sysProvinceService.getList();
-//        if(sysProvinceList!=null&&!sysProvinceList.isEmpty()){
-//            SysProvince sysProvince=  sysProvinceList.get(0);
-//            map.put("id", sysProvince.getId()+"");
-//            map.put("name", sysProvince.getName());
-//        }
-//        return map;
-//    }
-
 }
