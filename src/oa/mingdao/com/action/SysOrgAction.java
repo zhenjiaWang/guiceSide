f7539ca85303c1b4c312acd20fd3426e1acfb150
zhenjiaWang <zhenjiaWang@gmail.com>
2016-12-20 16:45:36 +0800

    123

--- src/oa/mingdao/com/action/SysOrgAction.java
+++ src/oa/mingdao/com/action/SysOrgAction.java
@@ -10,6 +10,8 @@ import org.guiceside.web.action.BaseAction;
 import org.guiceside.web.annotation.Action;
 import org.guiceside.web.annotation.ReqGet;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.List;
 
 
@@ -39,9 +41,32 @@ public class SysOrgAction extends BaseAction {
 
         TxcTransactionScaner txcTransactionScaner=new TxcTransactionScaner("o2-shop-.1758862495957920.BJ");
 
+        Class<?> cls = Class.forName(TxcTransactionScaner.class.getName());
 
-//        BeanUtils.forceSetProperty(txcTransactionScaner,"accessKey","");
-//        BeanUtils.forceSetProperty(txcTransactionScaner,"secretKey","");
+        //设定构造函数的参数类型
+        Class<?>[] parTypes=new Class<?>[1];
+        parTypes[0]=String.class;
+        //获取构造器
+        Constructor<?> con=cls.getConstructor(parTypes);//----------------重点注意，参数变化了
+
+        Object[] pars=new Object[1];
+        pars[0]="o2-shop-.1758862495957920.BJ";
+        //构造对象
+        Object txcObj=con.newInstance(pars);           //----------------重点注意，参数变化了
+        Class<?> obj = txcObj.getClass();
+        Field[] fields = obj.getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            fields[i].setAccessible(true);
+            if ("accessKey".equals(fields[i].getName())) {
+                fields[i].set(txcObj, "Y6G2pVpsa2wbloBQ");
+            }else if ("secretKey".equals(fields[i].getName())) {
+                fields[i].set(txcObj, "dlH4KDRz6poW4FCe6JaL8bOFifYYRn");
+            }else{
+                continue;
+            }
+        }
+
+
 
 
 
--- src/org/guiceside/web/dispatcher/mapper/DefaultActionMapper.java
+++ src/org/guiceside/web/dispatcher/mapper/DefaultActionMapper.java
@@ -257,16 +257,25 @@ public class DefaultActionMapper implements ActionMapper {
      */
     private static String parseSuffix(String url) {
 
+        String str=null;
         Matcher matcher = pattern.matcher(url);
 
         String[] spUrl = url.toString().split("/");
-        int len = spUrl.length;
-        String endUrl = spUrl[len - 1];
-
-        if(matcher.find()) {
-            String[] spEndUrl = endUrl.split("\\?");
-            return spEndUrl[0].split("\\.")[1];
+        if(spUrl!=null&&spUrl.length>0){
+            int len = spUrl.length;
+            String endUrl = spUrl[len - 1];
+            if(matcher.find()) {
+                String[] spEndUrl = endUrl.split("\\?");
+                str=spEndUrl[0];
+            }else {
+                str=endUrl;
+            }
+            if(str.indexOf(".")!=-1){
+                str=str.split("\\.")[1];
+            }else{
+                str=null;
+            }
         }
-        return endUrl.split("\\.")[1];
+        return str;
     }
 }
