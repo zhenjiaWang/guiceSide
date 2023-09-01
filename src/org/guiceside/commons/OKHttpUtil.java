c05d3a1ad5bca30cf6eee895d2c0223727ba5f47
wangjia <wangjiamr@163.com>
2023-09-01 17:28:03 +0800

    1

--- src/org/guiceside/commons/OKHttpUtil.java
+++ src/org/guiceside/commons/OKHttpUtil.java
@@ -1,25 +1,25 @@
 package org.guiceside.commons;
 
-import com.squareup.okhttp.*;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import okhttp3.*;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
+import org.guiceside.commons.lang.StringUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zhenjiaWang on 15/8/26.
  */
 public class OKHttpUtil {
-    private static final OkHttpClient okHttpClient = new OkHttpClient();
+    private static final OkHttpClient okHttpClient = new OkHttpClient.Builder().
+            connectTimeout(40, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS).build();
+    ;
 
-    static {
-        okHttpClient.setConnectTimeout(30, TimeUnit.SECONDS);
-    }
 
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
@@ -58,14 +58,11 @@ public class OKHttpUtil {
 
     public static String post(String url, Map<String, String> paramsMap) throws IOException {
         String responseStr = null;
-        FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
-        if (paramsMap != null && !paramsMap.isEmpty()) {
-            Set<String> keys = paramsMap.keySet();
-            for (String key : keys) {
-                formEncodingBuilder.add(key, paramsMap.get(key));
-            }
-        }
-        RequestBody formBody = formEncodingBuilder.build();
+
+        RequestBody formBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),
+                JSONObject.fromObject(paramsMap).toString());
+
+        //RequestBody formBody = formEncodingBuilder.build();
 
         Request request = new Request.Builder()
                 .url(url)
@@ -135,5 +132,29 @@ public class OKHttpUtil {
             throw new IOException("Unexpected code " + response);
         }
         return responseStr;
+    }
+
+    public static void main(String []args)throws Exception{
+        Map<String, JSONObject> map = new HashMap<>();
+        String url = "https://open.lixinger.com/api/cn/company";
+        JSONObject prams = new JSONObject();
+        prams.put("token", "e435e1bf-1287-4ffb-b1c8-6343b34689a1");
+        prams.put("includeDelisted", true);
+        String r = OKHttpUtil.post(url, prams.toString());
+        if (StringUtils.isNotBlank(r)) {
+            JSONObject result = JSONObject.fromObject(r);
+            if (result.containsKey("code") && result.getInt("code") == 1 && result.containsKey("data")) {
+                JSONArray array = result.getJSONArray("data");
+                if (!array.isEmpty()) {
+                    for (Object obj : array) {
+                        JSONObject object = (JSONObject) obj;
+                        map.put(object.getString("stockCode"), object);
+                    }
+                }
+            }
+        } else {
+            System.out.println(r);
+        }
+        System.out.println(map.size());
     }
 }
